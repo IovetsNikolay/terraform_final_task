a79a2ebaa352217e5b2e8de1433b89b1e997d4b0
IovetsNikolay <indicow@gmail.com>
2020-11-08 21:30:42 +0200

    asdasdawds

--- main.tf
+++ main.tf
@@ -12,55 +12,55 @@ module "launch-template" {
   name = "tform_launch_template"
 }
 
-resource "aws_autoscaling_group" "main" {
-  availability_zones = ["us-east-1a"]
-  desired_capacity   = 2
-  max_size           = 2
-  min_size           = 2
+//resource "aws_autoscaling_group" "main" {
+//  availability_zones = ["us-east-1a"]
+//  desired_capacity   = 2
+//  max_size           = 2
+//  min_size           = 2
+//
+//  health_check_grace_period = 300
+//  health_check_type         = "ELB"
+//
+//  load_balancers = [aws_elb.main.name]
+//
+//  launch_template {
+//    id      = module.launch-template.launch_template_id
+//    version = "$Latest"
+//  }
+//}
 
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
+//resource "aws_autoscaling_policy" "main" {
+//  autoscaling_group_name = aws_autoscaling_group.main.name
+//  name = "Target Tracking Policy"
+//  policy_type = "TargetTrackingScaling"
+//
+//  target_tracking_configuration {
+//    predefined_metric_specification {
+//      predefined_metric_type = "ASGAverageCPUUtilization"
+//    }
+//
+//    target_value = 60.0
+//  }
+//}
 
-  load_balancers = [aws_elb.main.name]
-
-  launch_template {
-    id      = module.launch-template.launch_template_id
-    version = "$Latest"
-  }
-}
-
-resource "aws_autoscaling_policy" "main" {
-  autoscaling_group_name = aws_autoscaling_group.main.name
-  name = "Target Tracking Policy"
-  policy_type = "TargetTrackingScaling"
-
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-
-    target_value = 60.0
-  }
-}
-
-resource "aws_elb" "main" {
-  name               = "foobar-terraform-elb"
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 10
-    timeout             = 5
-    target              = "HTTP:80/index.html"
-    interval            = 30
-  }
+//resource "aws_elb" "main" {
+//  name               = "foobar-terraform-elb"
+//  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+//
+//  listener {
+//    instance_port     = 80
+//    instance_protocol = "http"
+//    lb_port           = 80
+//    lb_protocol       = "http"
+//  }
+//
+//  health_check {
+//    healthy_threshold   = 2
+//    unhealthy_threshold = 10
+//    timeout             = 5
+//    target              = "HTTP:80/index.html"
+//    interval            = 30
+//  }
 
 //  instances                   = [aws_instance.foo.id]
 //  cross_zone_load_balancing   = true
